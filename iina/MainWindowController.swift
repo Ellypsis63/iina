4c9f56c859124edb2aee7165c7b02ba2ccb0235d
low-batt <86170219+low-batt@users.noreply.github.com>
2021-12-30 17:43:42 -0500

    Fix crash in mpv_set_property during termination, #11
    This was also reported against IINA in issue iina#3596
    
    The commit in the pull request will:
    - Rename the PlayerCore property isMpvTerminated to isMpvTerminating to
      reflect that termination has been initiated and being processed
      asynchronously
    - Change existing references to  isMpvTerminated  to use new name
    - Change PlayerCore.terminateMPV to set isMpvTerminating to true right
      before starting the termination process
    - Change PlayerCore.syncUI to do nothing if mpv is terminating
    - Change MPVController.mpvQuit to remove observers MPVController
      registered before sending the quit command to mpv
    - Change many MPVController methods to check to see if mpv is being
      terminated

--- iina/MainWindowController.swift
+++ iina/MainWindowController.swift
@@ -1089,7 +1089,7 @@ class MainWindowController: PlayerWindowController {
       }
     }
     // stop playing
-    if !player.isMpvTerminated {
+    if !player.isMpvTerminating {
       if case .fullscreen(legacy: true, priorWindowedFrame: _) = fsState {
         restoreDockSettings()
       }
