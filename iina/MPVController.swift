002a61876e70fc6b70da3cc275bc4beffd20ea98
李通洲 <carter.li@eoitek.com>
2022-01-03 22:44:20 +0800

    Danmaku: Initial support ( experimental )

--- iina/MPVController.swift
+++ iina/MPVController.swift
@@ -91,7 +91,8 @@ class MPVController: NSObject {
     MPVOption.Window.windowScale: MPV_FORMAT_DOUBLE,
     MPVProperty.mediaTitle: MPV_FORMAT_STRING,
     MPVProperty.videoParamsRotate: MPV_FORMAT_INT64,
-    MPVProperty.idleActive: MPV_FORMAT_FLAG
+    MPVProperty.idleActive: MPV_FORMAT_FLAG,
+    MPVProperty.timePos: MPV_FORMAT_INT64
   ]
 
   private let isDestroyed = DispatchSemaphore(value: 0)
@@ -1055,6 +1056,12 @@ class MPVController: NSObject {
           player.closeMainWindow()
         }
         receivedEndFileWhileLoading = false
+      }
+
+    case MPVProperty.timePos:
+      DispatchQueue.main.async {
+        guard self.player.mainWindow.isWindowLoaded else { return }
+        self.player.mainWindow.updateDanmakuTime()
       }
 
     default:
