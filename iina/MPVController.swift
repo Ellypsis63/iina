23602bff450d75c655c721960a545294b82de82f
low-batt <86170219+low-batt@users.noreply.github.com>
2022-01-24 23:27:01 -0500

    Fix NSFileHandleOperationException crash, #3590
    This commit will:
    - Change AppDelegate.applicationShouldTerminate to use a copy of
      playerCores in the background thread
    - Add logging to AppDelegate.applicationShouldTerminate to debug a
      hang during termination
    - Add logging to MPVController methods mpvQuit and handleEvent to
      also help debug the problem

--- iina/MPVController.swift
+++ iina/MPVController.swift
@@ -439,6 +439,7 @@ class MPVController: NSObject {
     removeObservers()
     // The quit command executes asynchronously. A MPV_EVENT_SHUTDOWN event is emitted when the quit
     // command finishes.
+    Logger.log("Destroying mpv context \(String(describing: mpvClientName))")
     command(.quit)
   }
 
@@ -663,6 +664,7 @@ class MPVController: NSObject {
         mpv_destroy(mpv)
         mpv = nil
         isDestroyed.signal()
+        Logger.log("Destroyed mpv context \(String(describing: mpvClientName))")
       }
 
     case MPV_EVENT_LOG_MESSAGE:
